792bc5fc8cf04f15fcf883a2a550c7e13e5e4526
Joshua-Fish-Student <jofid7d@student.oaisd.org>
2024-10-11 17:27:41 +0000

    did the assignment

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("What opperator (+, -, *, /, or modulus)?");
+        string opp = Console.ReadLine();
+        Console.WriteLine("First number?");
+        float num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Second number?");
+        float num2 = float.Parse(Console.ReadLine());
+        if (opp == "+"){
+            Console.WriteLine(num1 + " + " + num2 + " is " + (num1 + num2));
+        }
+        else if (opp == "-"){
+            Console.WriteLine(num1 + " - " + num2 + " is " + (num1 - num2));
+        }
+        else if (opp == "/"){
+            Console.WriteLine("Decimal or integer?");
+            string option = Console.ReadLine();
+            if (option == "integer"){
+                num1 = (int)num1;
+                num2 = (int)num2;
+            }
+            Console.WriteLine(num1 + " / " + num2 + " is " + num1 / num2);
+        }
+        else if (opp == "*"){
+            Console.WriteLine(num1 + " * " + num2 + " is " + (num1 * num2));
+        }
+        else if (opp == "modulus"){
+            Console.WriteLine(num1 + " modulus " + num2 + " is " + (num1 % num2));
+        }
+    }
+}
